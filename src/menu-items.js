b13af4e4e716c20c9ebebf44ebdb42e6c5c16a20
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-05-31 10:17:52 -0500

    create: menu item class for creation of menu items + first item created - start on first dropdown option

---
+++ src/menu-items.js
@@ -0,0 +1,39 @@
+export class menuItem {
+  constructor(name, text, imgSrc) {
+    this.name = name;
+    this.text = text;
+    this.imgSrc = imgSrc;
+  }
+
+  createItem() {
+    const item = document.createElement('div');
+
+    const imgCont = document.createElement('div');
+    const img = new Image();
+    img.src = `${this.imgSrc}`;
+    imgCont.appendChild(img);
+
+    const content = document.createElement('div');
+    content.classList = 'menuContent';
+
+    const title = document.createElement('h1');
+    title.classList = 'item-Title';
+    title.innerText = `${this.name}`;
+
+    const description = document.createElement('p');
+    description.classList = 'item-desc';
+    description.innerText = `${this.text}`;
+
+    content.append(
+      title,
+      description,
+    );
+
+    item.append(
+      imgCont,
+      content,
+    );
+
+    return item;
+  }
+}
--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,6 @@
 import { appendContent } from './initial';
+import Grass from './imgs/grass.jpg';
+import { menuItem } from './menu-items';
 
 export const menu = function () {
   const title = document.createElement('h1');
@@ -7,14 +9,16 @@ export const menu = function () {
   const subtext = document.createElement('h2');
   subtext.innerText = 'This is the greatest menu in all history';
 
-  const p1 = document.createElement('p');
-  p1.innerText = 'Testing the logic for this';
+  const menuContainer = document.createElement('div');
+  menuContainer.classList = 'menuContainer';
 
-  const callToAction = document.createElement('h3');
-  callToAction.innerText = 'Eat with us please';
+  const grass = new menuItem('Grass', 'The best grass you\'ve ever eaten', Grass);
+
+  console.log(grass);
+
+  menuContainer.appendChild(grass.createItem());
 
   appendContent(title, 'main');
   appendContent(subtext, 'main');
-  appendContent(p1, 'main');
-  appendContent(callToAction, 'main');
+  appendContent(menuContainer, 'main');
 };
