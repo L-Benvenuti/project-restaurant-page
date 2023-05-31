7a781bf61672a856d06e7cb6e504e570644a4c72
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-05-31 11:28:55 -0500

    create: select dropdown + logic to append menu chosen

--- src/menu.js
+++ src/menu.js
@@ -8,12 +8,41 @@ export const menu = function () {
   const subtext = document.createElement('h2');
   subtext.innerText = 'This is the greatest menu in all history';
 
-  // need to create the dropdown selection
-  // then if condition for each selection to have different outcomes
-  // if (selection === 'boring') {boring();}
+  // dropdown selection
+  const menuSelection = document.createElement('select');
+  menuSelection.id = 'menuOptions';
+
+  const placeholder = document.createElement('option');
+  placeholder.innerText = 'Select your favorite menu:';
+  placeholder.disabled = true;
+  placeholder.selected = true;
+  const boringMenu = document.createElement('option');
+  boringMenu.innerText = 'Boring Menu';
+  boringMenu.value = 'boring';
+  const interestingMenu = document.createElement('option');
+  interestingMenu.innerText = 'Interesting Menu';
+  interestingMenu.value = 'interesting';
+  const deathWishMenu = document.createElement('option');
+  deathWishMenu.innerText = 'Death Wish Menu';
+  deathWishMenu.value = 'death';
+
+  menuSelection.append(
+    placeholder,
+    boringMenu,
+    interestingMenu,
+    deathWishMenu,
+  );
+
+  menuSelection.addEventListener('change', () => { menuChange(); });
+
+  function menuChange() {
+    const selection = document.getElementById('menuOptions');
+    if (selection.value === 'boring') {
+      boring();
+    }
+  }
 
   appendContent(title, 'main');
   appendContent(subtext, 'main');
-
-  boring();
+  appendContent(menuSelection, 'main');
 };
