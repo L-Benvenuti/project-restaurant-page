d9a1dbd71f56f006d243c5441876cef24c6dfbcb
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-05-31 13:51:12 -0500

    create: interesting menu - still need to change meal descriptions

--- src/menu/interesting-dropdown.js
+++ src/menu/interesting-dropdown.js
@@ -1,4 +1,10 @@
 import { menuItem } from './menu-items';
+import Tacos from '../imgs/food-items/interesting/tacos.png';
+import Pizza from '../imgs/food-items/interesting/pizza.png';
+import Wings from '../imgs/food-items/interesting/wings.png';
+import Pasta from '../imgs/food-items/interesting/pasta.png';
+import Cknfries from '../imgs/food-items/interesting/nuggies-fries.png';
+import Burger from '../imgs/food-items/interesting/burger.png';
 
 export const interesting = () => {
   const iMenu = document.createElement('div');
@@ -6,30 +12,30 @@ export const interesting = () => {
 
   const title = document.createElement('h1');
   title.classList = 'menuTitle';
-  title.innerText = 'Boring Menu';
+  title.innerText = 'Interesting Menu';
 
   const subtext = document.createElement('p');
   subtext.classList = 'menuSubt';
-  subtext.innerText = 'I guess if you enjoy super boring food, these are for you'
-      + '\nConsider these our low carbs (whatever that means...)';
+  subtext.innerText = 'Now we\'re talking! Nothing like some human food!'
+      + '\nEnjoy every last bite!';
 
-  const grass = new menuItem('Grass', 'The best grass you\'ve ever eaten', Grass);
-  const grains = new menuItem('Grains', 'If you feel constipated, I\'ve heard this helps', Grains);
-  const melon = new menuItem('Melon', 'Best fruit on the entire planet - better than the one your mom gets you', Melon);
-  const reeds = new menuItem('Reeds', 'Not even sure what this is, but yeah... delicious', Reeds);
-  const squash = new menuItem('Squash', 'Pretty good pumpkin wannabe', Squash);
-  const waterPlant = new menuItem('Water Plants', 'Another delicious green thing', WaterPlants);
+  const tacos = new menuItem('Tacos', 'The best grass you\'ve ever eaten', Tacos);
+  const pizza = new menuItem('Pizza', 'If you feel constipated, I\'ve heard this helps', Pizza);
+  const wings = new menuItem('Wings', 'Best fruit on the entire planet - better than the one your mom gets you', Wings);
+  const pasta = new menuItem('Spaghetti Meatballs', 'Not even sure what this is, but yeah... delicious', Pasta);
+  const cknfries = new menuItem('Nuggies & Fries', 'Pretty good pumpkin wannabe', Cknfries);
+  const burger = new menuItem('Juicy Burger', 'Another delicious green thing', Burger);
 
   iMenu.append(
     title,
     subtext,
-    grass.createItem(),
-    grains.createItem(),
-    melon.createItem(),
-    reeds.createItem(),
-    squash.createItem(),
-    waterPlant.createItem(),
+    tacos.createItem(),
+    pizza.createItem(),
+    wings.createItem(),
+    pasta.createItem(),
+    cknfries.createItem(),
+    burger.createItem(),
   );
 
-  return iMenuMenu;
+  return iMenu;
 };
